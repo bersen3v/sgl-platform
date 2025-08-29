a6867dac548fcc0269c4802005ed56704b92e09c
IvanBersenev <bersenev4dev@gmail.com>
2025-08-29 22:48:48 +0700

    url

--- frontend/src/shared/network/config/baseUrl.ts
+++ frontend/src/shared/network/config/baseUrl.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://localhost:8090";
 // export const baseUrl = "https://sgl-rights-go-backend-1.onrender.com";
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = "/api";
